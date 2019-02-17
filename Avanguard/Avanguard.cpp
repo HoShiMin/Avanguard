4026af262652ddf1bbf9de187e518808efa3721c
HoShiMin <aleksanderem@yandex.ru>
2019-02-17 21:27:12 +0300

    Initial commit

---
+++ Avanguard/Avanguard.cpp
@@ -0,0 +1,15 @@
+﻿#include <Windows.h>
+
+#pragma comment(lib, "ntdll.lib")
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD dwReason, PCONTEXT lpContext)
+{
+    switch (dwReason) {
+    case DLL_PROCESS_ATTACH:
+        break;
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+
+    return TRUE;
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Avanguard
